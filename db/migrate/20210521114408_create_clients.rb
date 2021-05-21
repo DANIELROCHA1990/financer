b802f0fd2e8612f1c0488c0284e0443061420953
Daniel Rocha <daniel1990rocha@gmail.com>
2021-05-21 09:11:46 -0300

    first commit

---
+++ db/migrate/20210521114408_create_clients.rb
@@ -0,0 +1,18 @@
+class CreateClients < ActiveRecord::Migration[6.1]
+  def change
+    create_table :clients do |t|
+      t.string :name
+      t.string :company_name
+      t.string :doument
+      t.string :string
+      t.string :email
+      t.string :phone
+      t.references :user, null: false, foreign_key: true
+      t.text :notes
+      t.string :status
+      t.string :integer
+
+      t.timestamps
+    end
+  end
+end
